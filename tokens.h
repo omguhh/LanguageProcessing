952ec791532abf24c1f7304101dc0df22bf47ffc
Yasmine <shroomz.ftw@gmail.com>
2015-03-18 11:08:43 +0400

    got my token definitions onnnn

---
+++ tokens.h
@@ -0,0 +1,32 @@
+#define procedure 13
+#define is 1
+#define begin 2
+#define end 3
+#define if 4
+#define then 5
+#define else 6
+#define end if 7
+#define for 8
+#define in 9
+#define loop 10
+#define end loop 11
+
+#define ID 14
+#define INT 15
+#define PLUS 16
+#define MINUS 17
+#define STAR 18
+
+#define LT 19
+#define GT 20
+
+#define EQ 21
+#define NEQ 22
+
+#define GTE 23
+#define LTE 24
+#define ASSIGN 25
+#define SEMI 26
+
+
+#define MAXIDENT 256
