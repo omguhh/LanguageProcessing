cc793ba96f1183480a2bf8b56a914e3575dd19f4
Yasmine <shroomz.ftw@gmail.com>
2015-03-21 17:02:02 +0400

    😢 😢

--- print.c
+++ print.c
@@ -6,43 +6,45 @@ extern int symb;
 extern char * yytext;
 
 char * showSymb(int symb)
-{  switch(symb)
-   {  	case  PROCEDURE: return "procedure";
-      	case  COlO: return ":";
-	case SEMI: return ";";
-	case ASSIGN: return ":=";
-	case IF: return "if";
-	case ENDIF: return "end if";
-	case THEN: return "then";
-	case END: return "end";
-	case DO:return "do";
-	case IS:return "is";
-	case FOR:return "for";
-	case IN:return "in";
-	case LOOP:return "loop";
-	case ENDLOOP:return "endloop";
+{  switch(symb)   {  	
+  case PROCEDURE: return "procedure";
+  case IS: return "is";
+  case BEGIN: return "begin";
+  case END: return "end";
+  case SEMI: return ";";
+  case ASSIGN: return ":=";
 
-      	case  PLUS: return "+";
-      	case  MINUS: return "-";
-      	case  STAR: return "*";
-      	case  SLASH: return "/";
-     	case  LBRA: return "(";
-      	case  RBRA: return ")";
-      	case  LT: return "<";
-      	case  LTE: return "<=";
-      	case  EQ: return "=";
-      	case  NEQ: return "!=";
-      	case  GTE: return ">=";
-      	case  GT: return ">";
-      	case EOF: return "EOF";
-      	default: printf("bad symbol: %d",symb);
+  case NUMBER: return "INT";
+  case NAME: return "NAME";
+
+  case IF: return "if";
+  case THEN: return "then";
+  case ELSE: return "else";
+  case ENDIF: return "end if";
+  case FOR: return "for";
+  case IN: return "in";
+  case TO: return "to";
+  case LOOP: return "loop";
+  case ENDLOOP: return "endloop";
+
+  case LT:return "<";
+  case EQ: return "=";
+  case LTE: return "<=";
+  case NEQ: return "\=";
+
+  case PLUS: return "+";
+  case MINUS: return "-";
+  case STAR: return "*";
+  case COLO: return ":";
+
+  default: printf("bad symbol: %d",symb);
    }
 }
 
 printSymb()
 {  char * s;
    printf("%s ",showSymb(symb));
-   if(symb==ID || symb==INT){
+   if(symb==NAME || symb==NUMBER){
     if(yytext == NULL)
       printf("Error: yytext is null");
     else
